08815d56b6541a488fecfdeafc548a45a3f991fb
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-10-10 12:20:21 -0300

    Refactoring for using mapstruct

--- src/main/java/com/digitalInovationOne/personapi/service/PersonService.java
+++ src/main/java/com/digitalInovationOne/personapi/service/PersonService.java
@@ -4,6 +4,7 @@ import com.digitalInovationOne.personapi.dto.request.PersonDTO;
 import com.digitalInovationOne.personapi.dto.response.MessageResponseDTO;
 import com.digitalInovationOne.personapi.entity.Person;
 import com.digitalInovationOne.personapi.exception.PersonNotFoundException;
+import com.digitalInovationOne.personapi.mapper.PersonMapper;
 import com.digitalInovationOne.personapi.repository.PersonRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,22 +18,28 @@ import java.util.stream.Collectors;
 public class PersonService {
 
     private PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
+    // Metodo refatorado o uso do Mapstruct
     public List<PersonDTO> listAll() {
         List<Person> allPeople = personRepository.findAll();
-        List<PersonDTO> listDto = allPeople.stream().map(obj -> new PersonDTO(obj)).collect(Collectors.toList());
-        return listDto;
+//        return allPeople.stream().map(obj -> new PersonDTO(obj)).collect(Collectors.toList());
+        return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 
+    // Metodo refatorado o uso do Mapstruct
     public MessageResponseDTO createPerson(PersonDTO personDTO){
-        Person personToSave = new Person(personDTO);
+//        Person personToSave = new Person(personDTO);
+        Person personToSave = personMapper.toModel(personDTO);
         Person savedPerson = personRepository.save(personToSave);
         return createMessageResponse(savedPerson.getId(), "Created person with ID ");
     }
 
+    // Metodo refatorado o uso do Mapstruct
     public PersonDTO findById(Long id) throws PersonNotFoundException {
         Person person = verifyIfExists(id);
-        return new PersonDTO(person);
+//        return new PersonDTO(person);
+        return personMapper.toDTO(person);
     }
 
     public void delete(Long id) throws PersonNotFoundException{
@@ -40,9 +47,11 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
+    // Metodo refatorado o uso do Mapstruct
     public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
         verifyIfExists(id);
-        Person personToUpdate = new Person(personDTO);
+//        Person personToUpdate = new Person(personDTO);
+        Person personToUpdate = personMapper.toModel(personDTO);
         Person updatedPerson = personRepository.save(personToUpdate);
         return createMessageResponse(updatedPerson.getId(), "Updated person with ID ");
     }
